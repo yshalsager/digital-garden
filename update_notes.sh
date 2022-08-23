88b2678de08734d827f979d466ba896e4038eb5a
yshalsager <ysh-alsager@hotmail.com>
2022-08-23 14:30:05 +0200

    update_notes: flatten nested tags
    Signed-off-by: yshalsager <ysh-alsager@hotmail.com>

--- update_notes.sh
+++ update_notes.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 cd content/
-find . -name "*.md" -exec perl -pi -e 's|^created: (\d+-\d+-\d+) (\d+:\d+)|date: \1|g' -i '{}' \; -exec perl -pi -e 's|^updated: (\d+-\d+-\d+) (\d+:\d+)(?:.*)?|lastmod: \1|g' -i '{}' \;
+find . -name "*.md" -exec perl -pi -e 's|^created: (\d+-\d+-\d+) (\d+:\d+)|date: \1|g' -i '{}' \; -exec perl -pi -e 's|^updated: (\d+-\d+-\d+) (\d+:\d+)(?:.*)?|lastmod: \1|g' -i '{}' \; -exec perl -pi -e 's|#(\S+?)/(\S+)|#\1_\2|g' -i '{}' \;
 cd ..
