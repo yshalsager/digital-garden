cef84856ae8f9806987da96862aede852c5ce18b
Youssif Shaaban Alsager <ysh-alsager@hotmail.com>
2022-08-26 15:54:01 +0200

    Update update_notes.sh

--- update_notes.sh
+++ update_notes.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 cd content/
+# find and replace datetime with date, and nested tags
 find . -name "*.md" -exec perl -pi -e 's|^created: (\d+-\d+-\d+) (\d+:\d+)|date: \1|g' -i '{}' \; -exec perl -pi -e 's|^updated: (\d+-\d+-\d+) (\d+:\d+)(?:.*)?|lastmod: \1|g' -i '{}' \; -exec perl -pi -e 's|#(\S+?)/(\S+)|#\1_\2|g' -i '{}' \;
 cd ..
