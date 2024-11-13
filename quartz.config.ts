05107960e83df46fd9bd15940aa360c110a302f4
yshalsager <ysh-alsager@hotmail.com>
2024-11-13 21:25:20 +0200

    fix: correct date detection from frontmatter and git

--- quartz.config.ts
+++ quartz.config.ts
@@ -56,7 +56,7 @@ const config: QuartzConfig = {
     transformers: [
       Plugin.FrontMatter(),
       Plugin.CreatedModifiedDate({
-        priority: ["frontmatter", "filesystem"],
+        priority: ["frontmatter", "git", "filesystem"],
       }),
       Plugin.SyntaxHighlighting({
         theme: {
