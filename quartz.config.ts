85b3c874c00179b591826f402b09f7a1a1858304
yshalsager <ysh-alsager@hotmail.com>
2024-11-13 21:17:27 +0200

    Revert "Enable social images"
    This reverts commit 9863cc44bb503316df48b41a95129c8a8cc4c509.

--- quartz.config.ts
+++ quartz.config.ts
@@ -17,12 +17,7 @@ const config: QuartzConfig = {
     baseUrl: "notes.yshalsager.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
-    generateSocialImages: {
-      colorScheme: "darkMode", // what colors to use for generating image, same as theme colors from config, valid values are "darkMode" and "lightMode"
-      width: 1200, // width to generate with (in pixels)
-      height: 630, // height to generate with (in pixels)
-      excludeRoot: false, // wether to exclude "/" index path to be excluded from auto generated images (false = use auto, true = use default og image)
-    },
+    generateSocialImages: false,
     theme: {
       fontOrigin: "googleFonts",
       cdnCaching: true,
